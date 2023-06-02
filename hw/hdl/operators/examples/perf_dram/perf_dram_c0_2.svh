011e243bc7f7ef9a583141a6b8dc103399950c1a
zhe <zhe@ethz.ch>
2023-06-02 09:13:18 +0200

    change the naming convention of card and strm interface in the user examples

--- hw/hdl/operators/examples/perf_dram/perf_dram_c0_2.svh
+++ hw/hdl/operators/examples/perf_dram/perf_dram_c0_2.svh
@@ -5,8 +5,8 @@ always_comb axi_ctrl.tie_off_s();
 AXI4SR axis_sink_int ();
 AXI4SR axis_src_int ();
 
-axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_card_sink), .m_axis(axis_sink_int));
-axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_card_src));
+axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_card_0_sink), .m_axis(axis_sink_int));
+axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_card_0_src));
 
 // UL
 always_comb begin
