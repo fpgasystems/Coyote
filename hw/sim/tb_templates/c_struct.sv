709e0d85d15eb70112ca3585f3ae274210014df6
Michael Egloff <miegloff@student.ethz.ch>
2025-02-18 10:21:18 +0100

    variables removed because they were defined differently than in lynx_pkg_tmplt

--- hw/sim/tb_templates/c_struct.sv
+++ hw/sim/tb_templates/c_struct.sv
@@ -1,12 +1,4 @@
 package simTypes;
-
-    // STRM TYPES
-    parameter integer STRM_CARD = 0;
-    parameter integer STRM_HOST = 1;
-    parameter integer STRM_RREQ = 2;
-    parameter integer STRM_RRSP = 3;
-    parameter integer STRM_TCP  = 4;
-
     // --------------------------------------------------------------------------
     // CUSTOM STRUCTS
     // These are the structs to edit to adapt to any custom DUT.
