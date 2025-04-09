56e13091a570e88bb7403837e415877bf5651a7e
michlijordan <michie5436@gmail.com>
2025-04-09 22:06:18 +0200

    added build_files

---
+++ sim/build_files/build_sim_07.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# some variables to influence the behaviour of this script
+MAKE_JOBS="-j12" # probably enough, I havent seen make use this many even if allowed to
+CMAKE="/usr/bin/cmake"
+MAKE="make"
+
+# default options
+CLEAN_SIM_DIRECTORY=0;
+MAKE_SIM=0;
+NOGUI=0;
+
+# options
+OPTIONS=(
+		"c,clean,,Clean the sim directory"
+		"s,sim,,create the simulation project"
+		",nogui,,do not attempt to open vivado"
+	)
+LENGTHS=( 0 0 0 )
+SHORT_OPTIONS=""
+LONG_OPTIONS=""
+for LINE in "${OPTIONS[@]}"; do
+	ARR=( $(echo "$LINE" | awk -F ',' '{print "<"$1"> <"$2"> <"$3">" }') );
+		CLENS=( "${#ARR[0]}" "${#ARR[1]}" "${#ARR[2]}" );
+	if [[ "${LENGTHS[0]}" -lt "${CLENS[0]}" ]]; then
+		LENGTHS[0]="${CLENS[0]}"
+	fi
+	if [[ "${LENGTHS[1]}" -lt "${CLENS[1]}" ]]; then
+		LENGTHS[1]="${CLENS[1]}"
+	fi
+	if [[ "${LENGTHS[2]}" -lt "${CLENS[2]}" ]]; then
+		LENGTHS[2]="${CLENS[2]}"
+	fi
+	OPT_SHORT=$(echo "$LINE" | cut -f 1 -d ',')
+	OPT_LONG=$(echo "$LINE" | cut -f 2 -d ',')
+	OPT_ARG=$(if ! [[ "${ARR[2]}" = "<>" ]]; then echo ":"; fi)
+	
+	SHORT_OPTIONS=$SHORT_OPTIONS$OPT_SHORT$OPT_ARG
+	LONG_OPTIONS=${LONG_OPTIONS:+$LONG_OPTIONS,}$OPT_LONG$OPT_ARG
+	
+done
+
+function usage() {
+	echo "Usage:"
+	echo "  ./build.sh [options...]"
+	echo ""
+	echo "Options:"
+	for OPT_STRING_ENTRY in "${OPTIONS[@]}"; do
+		SHORT_OPT=$(echo "$OPT_STRING_ENTRY" | awk -F ',' '{print $1}');
+		LONG_OPT=$(echo "$OPT_STRING_ENTRY" | awk -F ',' '{print $2}');
+		ARG=$(echo "$OPT_STRING_ENTRY" | awk -F ',' '{ print "<"$3">" }');
+		DESC=$(echo "$OPT_STRING_ENTRY" | cut -f 4- -d ',');
+		ARG=$(if [[ "$ARG" = "<>" ]]; then echo ""; else echo "$ARG"; fi);
+		FORMAT_STRING="  -%s %s, --%s %s %$((("${LENGTHS[1]}"+"${LENGTHS[2]}"+"${LENGTHS[2]}"-"${#ARG}"-"${#ARG}"-"${#LONG_OPT}")))s %s\n"
+		printf "$FORMAT_STRING" "$SHORT_OPT" "$ARG" "$LONG_OPT" "$ARG" "" "$DESC"
+	done
+}
+
+function parse_args() {
+	# parse commandline options
+	OLD_OPTS=("$@")
+	SCREEN_OPTS=( )
+	eval set -- "$(getopt -l $LONG_OPTIONS -o $SHORT_OPTIONS -a -- "$@")"
+	while true; do
+		case "$1" in
+			-c|--clean) CLEAN_SIM_DIRECTORY=1; shift;;
+			-s|--sim) MAKE_SIM=1; shift;;
+			--nogui) NOGUI=1; shift;;
+			--) shift; break;;
+			
+			*) echo "Unimplemented Option: '$1'"; exit 1;;
+		esac
+	done
+	
+	# remove --background or -B options along with their arguments
+	function aux() {
+		while [[ $# -gt 0 ]]; do
+			if [[ "$1" = "-b" ]] || [[ "$1" = "--background" ]]; then
+				shift;
+				if ! [[ "$1" = "-"* ]]; then
+					shift;
+				fi
+			else
+				SCREEN_OPTS=(${SCREEN_OPTS[0]+"${SCREEN_OPTS[@]}"} "$1");
+				shift;
+			fi
+		done
+	}
+	
+	aux "${OLD_OPTS[@]}";
+}
+
+function create_log_file() {
+	LOG_FILE="$ABS_PATH/build-log-$1-$(date +%Y-%m-%d_%H:%M:%S).log"
+	touch "$LOG_FILE"
+}
+
+function clean_sim() {
+	# Clean the sim directory
+	rm -rf "$ABS_PATH"/../../build_sim;
+}
+
+function make_sim() {
+	# first create and change to sim directory
+	echo "Creating sim directory..."
+    if [[ ! -d "$ABS_PATH/../../build_sim" ]] || [[ -z $(ls "$ABS_PATH/../../build_sim") ]]; then
+        BUILD_DIR_EMPTY=1
+    fi
+	mkdir -p "$ABS_PATH/../../build_sim";
+	cd "$ABS_PATH/../../build_sim";
+
+	# running cmake			!!!ADJUST PATH!!!
+    if [[ "$CLEAN_SIM_DIRECTORY" -eq 1 ]] || [[ "$BUILD_DIR_EMPTY" ]]; then
+        "$CMAKE" "$ABS_PATH/../../examples/07_perf_fpga/hw" "-DFDEV_NAME=u55c" >> "$LOG_FILE" 2>&1
+        "$MAKE" "$MAKE_JOBS" project >> "$LOG_FILE" 2>&1
+    fi
+
+	
+	# creating simulation
+	echo "Running make sim..."
+	"$MAKE" "$MAKE_JOBS" sim >> "$LOG_FILE" 2>&1
+	
+	# patch simulation project				!!!ADJUST PROJECT NAME AND PATH!!!
+	echo "Patching simulation project"
+	vivado -mode tcl sim/example_07_perf_fpga.xpr < "$ABS_PATH/sim_patch_07.tcl" >> "$LOG_FILE" 2>&1
+	
+	if [[ "$NOGUI" -eq 0 ]]; then
+		# open vivado						!!!ADJUST PROJECT NAME!!!
+		echo "Opening Vivado..."
+		vivado "$ABS_PATH/../../build_sim/sim/example_07_perf_fpga.xpr" >> "$LOG_FILE" 2>&1
+    else
+        echo "Skipping Vivado..."
+	fi
+}
+
+function main() {
+	# parse arguments (note that this will already invoke the screen if necessary)
+	# all following code will run in the screen if that is wanted by the user
+	echo "Starting build script...";
+	parse_args "$@";
+	echo "Arguments parsed.";
+	
+	# Recover the absolute path of the repository, resolving symlinks
+	ABS_PATH="$(realpath $(dirname "$0"))";
+
+	# goto repository directory along the canonical path
+	cd "$ABS_PATH";
+	
+	# Clean the sim directory if requested
+	if [[ "$CLEAN_SIM_DIRECTORY" -eq 1 ]]; then
+		echo "Cleaning sim directory..."
+		clean_sim;
+	fi
+	
+	# run project setup and make sim if requested
+	if [[ "$MAKE_SIM" -eq 1 ]]; then
+		# create and name log file for sim build
+		create_log_file "sim";
+		
+		make_sim;
+	fi
+
+}
+
+
+main "$@";
---
+++ sim/build_files/sim_patch_02.tcl
@@ -0,0 +1,64 @@
+if {[catch {
+
+########################################################################################################
+
+# NOTE: this needs to be run with CWD in in the build directory and with the sim/test.xpr project open
+source "base.tcl" -notrace
+set proj [current_project]
+set sim_dir        "$build_dir/sim"
+
+########################################################################################################
+# Set project properties
+########################################################################################################
+#set_property "board_part" $board_part                      $proj
+set_property "default_lib" "xil_defaultlib"                 $proj
+set_property "ip_cache_permissions" "read write"            $proj
+set_property "ip_output_repo" "$sim_dir/$project.cache/ip"  $proj
+set_property "sim.ip.auto_export_scripts" "1"               $proj
+set_property "target_language" "Verilog"                    $proj
+set_property "simulator_language" "Mixed"                   $proj
+set_property "xpm_libraries" "XPM_CDC XPM_MEMORY"           $proj
+if {$cfg(en_pr) eq 1} {
+    set_property "pr_flow" "1"                              $proj
+}
+
+puts "**** Sim properties set"
+puts "****"
+
+########################################################################################################
+# Create and add source files
+########################################################################################################
+
+# Create 'sources_1' fileset (if not found)
+if {[string equal [get_filesets -quiet sources_1] ""]} {
+  create_fileset -srcset sources_1
+}
+
+#!!!ADJUST PATH!!!!
+source "$build_dir/../examples/02_hls_vadd/hw/src/init_ip.tcl" -notrace
+
+# add all the simulation files to the project         !!!ADJUST PATH!!!
+set obj [get_filesets sources_1]
+set files [list \
+  [ file normalize "$build_dir/example_02_hls_vadd_config_0/user_c0_0/hdl/lynx_pkg.sv" ] \
+  [ file normalize "$build_dir/example_02_hls_vadd_config_0/user_c0_0/hdl/vfpga_top.svh" ] \
+  [ file normalize "$build_dir/../sim"] \
+]
+add_files -fileset $obj $files
+
+# add wave behaviour            !!!ADJUST WAVEFORM!!!
+add_files -fileset sim_1 -norecurse [ file normalize "$build_dir/../sim_files/waveforms/tb_user_behav_02.wcfg"]
+
+# set necessary variables for the simulation
+# set_property verilog_define {simMemSegmentsDir=$build_dir/../memory_segments/} [get_filesets sim_1]
+set_property -name {xsim.simulate.runtime} -value {5000ns} -objects [get_filesets sim_1]
+
+########################################################################################################
+
+} errorstring]} {
+    puts "**** CERR: $errorstring"
+    puts "****"
+    exit 1
+}
+
+exit 0
