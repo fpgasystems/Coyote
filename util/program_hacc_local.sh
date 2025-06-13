c09e49657e8f5ab3f996d8a52f7940923fe38a78
Benjamin Ramhorst <benjamin.ramhorst@inf.ethz.ch>
2025-06-13 20:12:30 +0200

    Clean-up Coyote utility scripts for HACC

--- util/program_hacc_local.sh
+++ util/program_hacc_local.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+### A utility script to program a local FPGA with a Coyote bitstream and insert the driver on the ETHZ HACC cluster
+### This script should be executed from the node where the FPGA is (e.g., alveo-u55c-05, alveo-box-01, etc.) and from the Coyote/util directory
+
 # Check Vivado is available
-if ! [ -x "$(command -v vivado)" ]; then
-	echo "Vivado not found. Please make sure Vivado is installed."
-	exit 1
+ if ! [ -x "$(command -v vivado)" ]; then
+ 	echo "Vivado not found. Please make sure Vivado is installed."
+ 	exit 1
 fi
 
 #  Arguments: bistream path, driver path and device ID (optional)
@@ -10,27 +15,38 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 
-BIT_PATH=$1
-DRV_PATH=$2
+BITSTREAM_PATH=$1
+DRIVER_PATH=$2
 if [ -z "$3" ]; then
-    DEVICE=1
-    DEVICE=1
+  DEVICE=1
 else
-    DEVICE=$3
-    DEVICE=$3
+  DEVICE=$3
 fi
 
+# Environment set-up --- get IP and MAC address of the FPGA; convert to hex
+CLI_PATH=/opt/hdev/cli
+IP_ADDRESS=$($CLI_PATH/hdev get network -d 1 | awk '$1 == "1:" {print $2}')
+MAC_ADDRESS=$($CLI_PATH/hdev get network -d 1 | awk '$1 == "1:" {print $3}' | tr -d '()')
+IP_HEX=$($CLI_PATH/common/address_to_hex IP $IP_ADDRESS)
+MAC_HEX=$($CLI_PATH/common/address_to_hex MAC $MAC_ADDRESS)
+
 # Bitstream loading
-echo "** Programming the FPGA with $BIT_PATH"
-hdev program vivado -b $BIT_PATH -d $DEVICE
-echo " "
+echo "** Programming the FPGA with $BITSTREAM_PATH"
+$CLI_PATH/hdev program vivado -b $BITSTREAM_PATH -d $DEVICE
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to program the FPGA with the bitstream."
+  exit 1
+fi
 
 # Driver insertion
-echo "** Inserting the driver from $DRV_PATH"
-echo "** IP_ADDRESS: $DEVICE_1_IP_ADDRESS_HEX_0"
-echo "** MAC_ADDRESS: $DEVICE_1_MAC_ADDRESS_0"
-echo "y" | hdev program driver -i $DRV_PATH -p ip_addr=$DEVICE_1_IP_ADDRESS_HEX_0,mac_addr=$DEVICE_1_MAC_ADDRESS_0
-echo " "
+echo "** Inserting the driver from $DRIVER_PATH"
+echo "** FPGA IP_ADDRESS: $IP_HEX"
+echo "** FPGA MAC_ADDRESS: $MAC_HEX"
+echo "y" | $CLI_PATH/hdev program driver -i $DRIVER_PATH -p ip_addr=$IP_HEX,mac_addr=$MAC_HEX
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to insert Coyote driver."
+  exit 1
+fi
 
 # Final greetings
 echo "** It's Coyote after all, so thoughts & prayers!"
