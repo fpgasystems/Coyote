09fb230823b70275f2d560e25ea00a343ac9ebfc
Jonas Dann <jonas.dann@inf.ethz.ch>
2025-10-09 16:45:42 +0200

    Added idle randomization of signals to simulation

--- sim/hw/c_axil.svh
+++ sim/hw/c_axil.svh
@@ -77,10 +77,10 @@ class c_axil;
         axi.cbm.wstrb   <= ~0;
         axi.cbm.wvalid  <= 1'b1;
         @(axi.cbm iff (axi.cbm.awready == 1'b1 && axi.cbm.wready == 1'b1));
-        axi.cbm.awaddr  <= 0;
+        axi.cbm.awaddr  <= $urandom();
         axi.cbm.awvalid <= 1'b0;
-        axi.cbm.wdata   <= 0;
-        axi.cbm.wstrb   <= 0;
+        axi.cbm.wdata   <= $urandom();
+        axi.cbm.wstrb   <= $urandom();
         axi.cbm.wvalid  <= 1'b0;
 
         // Response
@@ -100,7 +100,7 @@ class c_axil;
         axi.cbm.araddr  <= addr;
         axi.cbm.arvalid <= 1'b1;
         @(axi.cbm iff (axi.cbm.arready == 1'b1));
-        axi.cbm.araddr  <= 0;
+        axi.cbm.araddr  <= $urandom();
         axi.cbm.arvalid <= 1'b0;
 
         // Response
