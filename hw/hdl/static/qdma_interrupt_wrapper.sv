16924e2cc2c9e261fdb2e360f1cecfb0c8f984d7
Benjamin Ramhorst <benjamin.ramhorst@inf.ethz.ch>
2026-01-22 00:57:09 +0100

    Versal QDMA handle mutliple concurrent interrupts

--- hw/hdl/static/qdma_interrupt_wrapper.sv
+++ hw/hdl/static/qdma_interrupt_wrapper.sv
@@ -29,40 +29,94 @@
  *
  * Maps the one-hot encoded interrupt signal from the shell to the correct format for the QDMA interrupt interface.
  * Each bit corresponds to the region (vFPGA) issuing the interrupt and is simply converted to a decimal number
- * irq_valid is set to true as long as one of the regions is requesting an interrupt.
  *
- * TODO (Versal): Can the QDMA handle more than 8 interrupts per PF in the QDMA? PR IRQ is 15 in Coyote by default...
- * TODO (Versal): How to handle concurrent interrupts? Would the shell ever encounter such a scenario?
+ * Additionally, this module ensures that only one interrupt is sent to the QDMA at a time, waiting for the driver to acknowledge
+ * the interrupt and clear the corresponding bit before sending the next one. Other DMA engines (e.g., XDMA) could support
+ * multiple interrupts being sent at the same time, but QDMA only supports one at a time.
  */
-module qdma_interrupt_wrapper (
-    input  logic [15:0] usr_irq, 
-    output logic        usr_irq_valid,
-    output logic [10:0] usr_irq_vec, 
-    output logic [12:0] usr_irq_fnc    
+module qdma_interrupt_wrapper #(
+    parameter integer N_IRQ = 16 
+) (
+    input  logic                aclk,
+    input  logic                aresetn,
+
+    input  logic [N_IRQ-1:0]    usr_irq, 
+    
+    input  logic                usr_irq_ack,
+    input  logic                usr_irq_fail,
+    output logic                usr_irq_valid,
+    output logic [4:0]          usr_irq_vec, 
+    output logic [12:0]         usr_irq_fnc    
 );
 
-    assign usr_irq_valid = |usr_irq;
-    assign usr_irq_fnc = 0;
+    // FSM:
+    // 1. IDLE: Wait for next interrupt
+    // 2. SEND: Interrupt sent to the QDMA, valid bit must remain high until ack is received
+    // 3. WAIT: Interrupt acked by QDMA, wait for driver to process and clear the corresponding bit
+    // 4. DONE: Driver cleared the interrupt bit, go back to IDLE
+    typedef enum logic[1:0]  {ST_IDLE, ST_SEND, ST_WAIT, ST_DONE} state_t;
+    logic [1:0] state_C, state_N;
 
-    always @(usr_irq)
-    case(usr_irq)
-        16'b0000000000000001 : usr_irq_vec = {6'b0, 5'd0};
-        16'b0000000000000010 : usr_irq_vec = {6'b0, 5'd1};
-        16'b0000000000000100 : usr_irq_vec = {6'b0, 5'd2};
-        16'b0000000000001000 : usr_irq_vec = {6'b0, 5'd3};
-        16'b0000000000010000 : usr_irq_vec = {6'b0, 5'd4};
-        16'b0000000000100000 : usr_irq_vec = {6'b0, 5'd5};
-        16'b0000000001000000 : usr_irq_vec = {6'b0, 5'd6};
-        16'b0000000010000000 : usr_irq_vec = {6'b0, 5'd7};
-        16'b0000000100000000 : usr_irq_vec = {6'b0, 5'd8};
-        16'b0000001000000000 : usr_irq_vec = {6'b0, 5'd9};
-        16'b0000010000000000 : usr_irq_vec = {6'b0, 5'd10};
-        16'b0000100000000000 : usr_irq_vec = {6'b0, 5'd11};
-        16'b0001000000000000 : usr_irq_vec = {6'b0, 5'd12};
-        16'b0010000000000000 : usr_irq_vec = {6'b0, 5'd13};
-        16'b0100000000000000 : usr_irq_vec = {6'b0, 5'd14};
-        16'b1000000000000000 : usr_irq_vec = {6'b0, 5'd15};
-        default              : usr_irq_vec = 11'b0; 
-    endcase
+    logic [4:0] idx_C, idx_N;
+
+    // REG
+    always_ff @(posedge aclk) begin
+        if (aresetn == 1'b0) begin
+            state_C <= ST_IDLE;
+            idx_C <= 0;
+        end else begin
+            state_C <= state_N;
+            idx_C <= idx_N;
+        end
+    end
+
+    // NSL
+    always_comb begin
+        state_N = state_C;
+        idx_N = idx_C;
+        
+        unique case(state_C)
+            ST_IDLE: begin
+                for (int i = 0; i < N_IRQ; i++) begin
+                    // Found next interrupt -> send to QDMA
+                    if (usr_irq[i]) begin
+                        idx_N = i;
+                        state_N = ST_SEND;
+                        break;
+                    end
+                end
+            end
+
+            ST_SEND: begin
+                // Interrupt acked by QDMA -> wait for driver to process 
+                if (usr_irq_ack) begin
+                    state_N = ST_WAIT;
+                end
+
+                // QDMA failed to accept interrupt -> go back to IDLE and (eventually) try again (driver can't clear bit if IRQ was never received)
+                if (usr_irq_fail) begin
+                    state_N = ST_IDLE;
+                end
+            end
+
+            ST_WAIT: begin
+                // Driver processed interrupt & cleared the active bit for the current vFPGA
+                // The bit is cleared by writing to a memory-mapped register in cnfg_slave_avx
+                if (usr_irq[idx_C] == 1'b0) begin
+                    state_N = ST_DONE;
+                end
+            end
+
+            ST_DONE: begin
+                // Extra timing state, nothing special; could be removed
+                state_N = ST_IDLE;
+            end
+        endcase
+    end
+
+    // DP
+    assign usr_irq_valid = (state_C == ST_SEND);
+    assign usr_irq_fnc = 0;
+    assign usr_irq_vec = idx_C;
 
 endmodule
